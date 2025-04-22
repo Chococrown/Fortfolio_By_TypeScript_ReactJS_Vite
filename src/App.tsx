b15584e4f0b4d449e02d3648551546db170495ca
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-04-22 11:45:30 +0700

    สร้างโปรเจค TypeScript + ReactJS

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='container'>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
