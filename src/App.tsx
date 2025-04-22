e7e3bdea1267f0e3c246c5737413e26ee7e02a14
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-04-22 18:51:54 +0700

    เพิ่ม icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import './App.css'
 
@@ -10,7 +7,7 @@ function App() {
   return (
     <>
 
-      <div className='container'>
+      <div className = 'container'>
         <div>
           <h1>Hello World</h1>
         </div>
